e99f6e1ead07aa0cc5e8229695c28ff2cbeff02d
Utkarsh Singh <utkarshsingh.0902@gmail.com>
2026-04-02 20:44:54 +0530

    Update JSON file path in config and add additional dependencies to requirements

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BASE_DIR=Path(__file__).resolve().parent
 
 DATA_DIR = BASE_DIR / "data"
 MODELS_DIR = BASE_DIR / "models"
-JSON_FILE = DATA_DIR / "response_2.json"
+JSON_FILE = DATA_DIR / "raw_supabase_data.json"
 
 def ensure_dirs():
     """Run this to make sure folders exist before saving files"""
